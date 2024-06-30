25a051e726930086a6cc82b7d910b0cbada61343
Humding3r <jetgeronimo@gmail.com>
2024-06-30 00:52:29 -0500

    Change default fish editor to nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,8 +9,12 @@ funcsave -q ls
 # turn off greeting
 set fish_greeting
 
+# change default editor to nvim
+set -Ux EDITOR nvim
+
 # add flutter to path
 fish_add_path -g -p /usr/bin/flutter/bin
+fish_add_path -g -p $HOME/.pub-cache/bin
 
 zoxide init --cmd cd fish | source
 starship init fish | source
