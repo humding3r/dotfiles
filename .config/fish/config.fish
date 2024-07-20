6351cb518125b21aab7d5382e534cebc2025209a
Humding3r <jetgeronimo@gmail.com>
2024-07-20 03:22:36 -0500

    Use tmux by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,14 @@ fish_add_path -g -p $HOME/.pub-cache/bin
 
 zoxide init --cmd cd fish | source
 starship init fish | source
+
+function start_tmux
+    if type tmux > /dev/null
+        #if not inside a tmux session, and if no session is started, start a new session
+        if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT
+            tmux -2 attach; or tmux -2 new-session
+        end
+    end
+end
+
+start_tmux
