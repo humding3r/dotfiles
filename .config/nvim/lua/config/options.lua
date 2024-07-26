95e4d42f3812b4996e8e4bbc86e8a733d7c2cc93
Humding3r <jetgeronimo@gmail.com>
2024-07-25 22:34:36 -0500

    Change nvim tab size to 2 spaces

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = false -- Turn off relative numbering
 vim.opt.wrap = false -- Turn off autowrapping
 vim.b.autoformat = false -- Turn off autoformatting
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 2 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
